7acaf0dc197e9793d616de051f8db607598c3cbc
dd3ok <hwick@nate.com>
2025-07-02 16:09:33 +0900

    fix: put -> patch로 변경

--- src/main/java/com/dd3ok/musinsatest/adapter/in/web/BrandCommandController.java
+++ src/main/java/com/dd3ok/musinsatest/adapter/in/web/BrandCommandController.java
@@ -11,9 +11,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,7 +36,7 @@ public class BrandCommandController {
         return ResponseEntity.created(location).build();
     }
 
-    @PutMapping("/{brandId}")
+    @PatchMapping("/{brandId}")
     public ResponseEntity<Void> updateBrand(@PathVariable Long brandId, @RequestBody BrandUpdateRequest request) {
         BrandUpdateCommand command = new BrandUpdateCommand(brandId, request.getName());
         brandCommandUseCase.updateBrand(command);
